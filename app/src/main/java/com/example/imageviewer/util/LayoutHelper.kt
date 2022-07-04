17af2785875df5a81d3a5281459ef9188e022ba6
nazanin <nazanin.omrani@gmail.com>
2022-07-04 09:51:32 +0430

    Added LayoutHelper.kt class for create LayoutParams for views

---
+++ app/src/main/java/com/example/imageviewer/util/LayoutHelper.kt
@@ -0,0 +1,48 @@
+package com.example.imageviewer.util
+
+import android.widget.FrameLayout
+
+class LayoutHelper {
+    companion object {
+        const val MATCH_PARENT = -1
+        const val WRAP_CONTENT = -2
+        private fun getSize(size: Int): Int {
+            return (if (size < 0) size else size.dp())
+        }
+
+        fun createFrame(
+            width: Int,
+            height: Int,
+            gravity: Int,
+            leftMargin: Int,
+            topMargin: Int,
+            rightMargin: Int,
+            bottomMargin: Int
+        ): FrameLayout.LayoutParams {
+            val layoutParams =
+                FrameLayout.LayoutParams(getSize(width), getSize(height), gravity)
+            layoutParams.setMargins(
+                leftMargin.dp(),
+                topMargin.dp(),
+                rightMargin.dp(),
+                bottomMargin.dp()
+            )
+            return layoutParams
+        }
+
+        fun createFrame(width: Int, height: Int, gravity: Int): FrameLayout.LayoutParams {
+            return FrameLayout.LayoutParams(
+                getSize(width),
+                getSize(height),
+                gravity
+            )
+        }
+
+        fun createFrame(width: Int, height: Int): FrameLayout.LayoutParams {
+            return FrameLayout.LayoutParams(
+                getSize(width),
+                getSize(height)
+            )
+        }
+    }
+}
